f38a973d864e844f63400f8b5fbd4280026625eb
ARIBILGI <ARIBILGI@BILCE>
2023-10-12 15:59:04 +0300

    Add project files.

---
+++ 02_VariableWorld/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace _02_VariableWorld
+{    
+
+    internal class Program
+    {
+
+        static void Main(string[] args) // Console programlarının ilk giriş noktası burasıdır.
+        {
+            // İlk tanımlamalar yapılıyor...
+
+            #region İlk örnek
+            //string name = ""; // ben burada hem değişgeni tanımladım hem de ilk değerini atatım
+
+            //string nameDesc;
+
+            //string Name;
+
+            //string NameDesc;
+
+            //// Değişgenlere değerler atanıyor....
+
+            //name = "Nurgül Karaçivi"; // name isimli değişgene değer atadım.
+
+            //nameDesc = "Benim adım : ";
+
+            //NameDesc = "Kızımın adı : ";
+
+            //Name = "Doğa Bengi KARAÇİVİ";
+
+            //// Şimdi bunları yazdıralım
+
+            //Console.WriteLine(nameDesc + name); // dynamic kullanım
+            //Console.WriteLine("Ümit KARAÇİVİ"); // static kullanım
+
+
+            #endregion
+
+            //#region değişgenlere devam
+            //// değişgenlere devam
+
+            //byte a = 100;
+
+            ////byte b = 300; byte sınırları aşan bir değer verildiği için hata verir.
+
+            //byte c = 200;
+
+            //int toplam = a + c;
+
+            //Console.WriteLine("Toplam = " + toplam.ToString()); // CW string yazabildiği için int bir değeri tostring ile karaktere ceviriyorum.
+
+            //#endregion
+
+
+            #region Ekrandan bilgi alma ve ekrana yazdırma, escape character kullanımı
+
+            //string namesname="";
+
+            //Console.WriteLine("Lütfen sizden rica etsem adınızı soyadınızı girer misiniz? \n\n");
+
+            //Console.WriteLine("\t\t");
+
+            //namesname=Console.ReadLine();
+
+            //Console.WriteLine("\n\n\nHoşgeldiniz...Sizi gördüğüme çok sevindim sayın " + namesname);
+
+            #endregion
+
+            // Sıklıkla yapılan hatalar..
+            //
+            // - Aynı satırda farklı veri türünden değişgen tanımlamaya çalışma
+
+            // int sayi1, string ad; yanlış kullanım.
+
+            // - Değişgene uygunsuz değer ataması yaparken
+
+            //int n;
+
+            //n = "ali";
+
+            // - değişgeni tanımlamadan ve/veya ilk değer vermeden değişgeni kullanmaya çalıştığımız
+
+            //int b = 5;
+
+            //Console.WriteLine(a);
+
+            // - Değişgen tanımlamasını ve/veya değer atamasını yanlış yerde yapma 
+
+            // - bazı değişgen türlerinde değişgenlere değer atarken bazı özel eklenmesi gereken belirteçleri eklememek (float)
+
+            decimal e = 25.4M;
+
+            Console.WriteLine(e);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 03_TypeConversions/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _03_TypeConversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bilinçsiz tür dönüşümü örnekleri
+
+            byte a=10;
+
+            short b = 20;
+
+            sbyte c = 30;
+
+            int d = a + b + c; // bilinçsiz tür dönüşümü otomatik olarak yapıldı
+
+            Console.WriteLine("Toplam = " + d.ToString() + "\n\n\n");
+
+            string e = "Arı Bilgi UCC09 Team";
+
+            char f = 'A';
+
+            object g = e + " " + f + " " + d; // farklı veri türleri tutabilen object tipi
+
+            long h = d;
+
+            float i = h;
+
+            double j = i;
+
+            double k = 21.6;
+
+            float l = (float)h + (float)k;
+
+            Console.WriteLine("1.durum : " + d.ToString() + "\n\n");
+
+            Console.WriteLine("2.durum : " + g.ToString() + "\n\n");
+
+            Console.WriteLine("3.durum : " + l.ToString() + "\n\n");
+
+            // Bilinçsiz Tür dönüşümünde ilginç bir durum - char veri tipi için
+            // 
+
+
+            char m = 'H';
+
+            int n = m;
+
+            Console.WriteLine("4.durum : " + n +"\n\n");
+
+            // Bilinçsiz tür dönüşüm örnekleri
+
+            int r = 15000;
+
+            byte s = (byte)r; // ????????? weekend arastırması
+
+            Console.WriteLine("5.durum : " + s + "\n\n");
+
+            Console.WriteLine("6.durum : " + r.ToString() + "\n\n");
+
+            Console.ReadKey();
+        }
+    }
+}
