66ded6a53b8988d91242cb17b5a22c8e8d50e1bd
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-10-12 16:11:33 +0300

    Update Program.cs

--- 03_TypeConversions/Program.cs
+++ 03_TypeConversions/Program.cs
@@ -52,7 +52,7 @@
 
             int r = 15000;
 
-            byte s = (byte)r; // ????????? weekend arastırması
+            byte s = (byte)r; // ????????? weekend araştırması
 
             Console.WriteLine("5.durum : " + s + "\n\n");
 
