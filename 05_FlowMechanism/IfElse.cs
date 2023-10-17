3bfb3aa831281fa2ca1f7f2446565f00d9f84657
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-10-17 10:57:51 +0300

    Some changes

--- 05_FlowMechanism/IfElse.cs
+++ 05_FlowMechanism/IfElse.cs
@@ -20,25 +20,49 @@ namespace _05_FlowMechanism
             Console.WriteLine("***************************************");
             Console.WriteLine("\n\n");
 
-            int a, b;
+            //int a, b;
 
-            a = Convert.ToInt32(Console.ReadLine()); // Console.Readline ekrandan sadece karakter okuması yapabildiği için bunu sayısal değere çevirmek için Convert kullanmak gerekiyor.
+            //a = Convert.ToInt32(Console.ReadLine()); // Console.Readline ekrandan sadece karakter okuması yapabildiği için bunu sayısal değere çevirmek için Convert kullanmak gerekiyor.
 
-            b = Convert.ToInt32(Console.ReadLine());
+            //b = Convert.ToInt32(Console.ReadLine());
 
-            if (a > b)
+            //if (a > b)
+            //{
+            //    Console.WriteLine("A sayısı : " + a);
+            //    Console.WriteLine("B sayısı : " + b);
+            //    Console.WriteLine("a sayısı b sayısından büyüktür....");
+            //}
+                
+                
+            //else 
+            //{ 
+            //    Console.WriteLine("a sayısı b sayısından küçüktür...."); 
+            //}
+
+            // Ekrandan alınan bir sayının negatif/pozitif/0 olup olmadığını ekranda "Girdiğiniz sayı Negatifdir/pozitifdir/Sıfırdır " şeklinde çıkış veren program parçası
+
+            int sayi;
+
+            Console.WriteLine("Lütfen sayınızı giriniz : ");
+            sayi=Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 0) // Önce sayının 0 dan büyüklüğü kontrol ediliyor..
             {
-                Console.WriteLine("A sayısı : " + a);
-                Console.WriteLine("B sayısı : " + b);
-                Console.WriteLine("a sayısı b sayısından büyüktür....");
+                Console.WriteLine("Girmiş olduğunuz sayı POZİTİFdir..");
             }
-                
-                
-            else 
-            { 
-                Console.WriteLine("a sayısı b sayısından küçüktür...."); 
-            }
+            else // Demek ki sayımız 0 dan küçük veya eşit dir
+            {
+                if (sayi < 0) // Hemen küçüklüğünü kontrol ediyorum.
+                {
+                    Console.WriteLine("Girmiş olduğunuz sayı NEGATİFdir..");
 
+                }
+                else
+                {
+                    Console.WriteLine("Girmiş olduğunuz sayı NÖTR dür..");
+                }
+            }
+   
 
         }
     }
