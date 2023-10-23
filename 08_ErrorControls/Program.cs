4496e980348126bf8323e899a6a3bd6448681a0e
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-10-23 19:01:21 +0300

    Some changes

--- 08_ErrorControls/Program.cs
+++ 08_ErrorControls/Program.cs
@@ -50,6 +50,69 @@
             }
 
             #endregion
+
+            // derste bahsetmiştik ama örnek yapmamış idik...
+            #region Hata Metotları
+
+            byte deger;
+
+            try
+            {
+                Console.Write("Byte tipinde bir değer girin: ");
+
+                deger = Convert.ToByte(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("-> Message: {0}\n-> Source: {1}\n-> StackTrace: {2}\n-> TargetSite: {3}\n",
+                    e.Message, e.Source, e.StackTrace, e.TargetSite);
+            }
+
+            #endregion
+
+
+            #region Başka bir örnek
+
+
+            int bolunecek, bolen;
+
+            try
+            {
+                Console.Write("-> Bölünecek Değeri Girin: ");
+
+                bolunecek = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("-> Bölen Değeri Girin: ");
+
+                bolen = Convert.ToInt32(Console.ReadLine());
+
+                double sonuc = (bolunecek / bolen);
+
+                Console.WriteLine("-> {0} değerinin {1} değerine bölümünün sonucu: {3}", bolunecek, bolen, sonuc);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("\n-> Hata! Sıfıra bölüm işlemi gerçekleştirilemez.\n-> Hata Mesajı: {0}\n", e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("\n-> Hata! Sadece Int32 türünde veri girişi yapabilirsiniz.\n-> Hata Mesajı: {0}\n", e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("\n-> Hata! Değişgen sınırların aştınız.\n-> Hata Mesajı: {0}\n", e.Message);
+            }
+            catch (Exception e) // Geri kalan tüm hata durumlarını kapsar
+            {
+                Console.WriteLine("\n-> Beklenmedik Bir Hata!\n-> Hata Mesajı: {0}\n", e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("-> Finally Bloğu Çalışıyor.\n");
+            }
+
+            #endregion
         }
     }
 }
