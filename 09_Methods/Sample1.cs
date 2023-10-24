6085fe2947d4e77e428b69b2a95690e4e1917dc9
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-10-24 11:08:22 +0300

    Some changes

--- 09_Methods/Sample1.cs
+++ 09_Methods/Sample1.cs
@@ -13,14 +13,27 @@ namespace _09_Methods
             // Dikdörtgenin alanını hesaplayan bir metodun 4 farklı şekilde kullanılması
 
             #region 1.Geriye değer döndürmeyen ve parametre almayan metot
-            Hesapla();
+            //Hesapla();
             #endregion
 
             #region 2.Geriye değer döndürmeyen ve parametre alan metot
+            // Kullanıcıdan giriş alarak.
+
+            //Console.WriteLine("Kısa kenar : ");
+            //int kk = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Uzun kenar : ");
+            //int uk = Convert.ToInt32(Console.ReadLine());
+
+            //Hesapla(kk, uk);
+
 
             #endregion
 
             #region 3.Geriye değer döndüren ve parametre almayan metot
+
+            int sonuc = HesaplaPrmsiz();
+
+            Console.WriteLine("Alan : {0}",sonuc);
 
             #endregion
 
@@ -42,6 +55,27 @@ namespace _09_Methods
             int alan = kk * uk;
 
             Console.WriteLine("Alan : {0} dır", alan );
+        }
+
+        private static void Hesapla(int kisa,int uzun)
+        {
+            int alan = kisa * uzun;
+
+            Console.WriteLine("Dikdörtgen alanı (metot-geri dönüş yok-parametre var..direkt buradan basıyor) {0}dır", alan);
+        }
+
+        private static int HesaplaPrmsiz()
+        {
+            // Geriye bir değer döndürecek metotlarda mutlaka ve mutlaka bir return keyword ü olmalıdır ki değeri çağrılan yere postalayabilsin.
+
+            Console.WriteLine("Kısa kenar : ");
+            int kk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Uzun kenar : ");
+            int uk = Convert.ToInt32(Console.ReadLine());
+
+            int alan = kk * uk;
+
+            return alan;
         }
     }
 }
